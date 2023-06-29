181929c803d0a4ea3005e00678b770c11820f984
Cristina Alvarez <alvarezcrisstinita@gmail.com>
2023-06-29 08:36:04 -0500

    Primer prueba

---
+++ cypress/e2e/spec_site.cy.js
@@ -0,0 +1,23 @@
+describe('instagram',()=>{
+  it('revisar',() =>{
+    cy.visit('http://instagram.com')
+  })
+})
+
+describe('facebook',()=>{
+  it('revisar',()=>{
+    cy.visit('http://facebook.com')
+  })
+})
+
+describe('duolingo',()=>{
+  it('visitar',()=>{
+    cy.visit('http://duolingo.com')
+  })
+})
+
+describe('snapchat',()=>{
+  it('visitar',()=>{
+    cy.visit('http://snapchat.com')
+  })
+})
